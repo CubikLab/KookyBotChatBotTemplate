15ddf06bfc3ac4d3718b68d0dec1b7426f3f70ad
Cubik65536 <cubik65536@cubik65536.top>
2022-07-23 01:57:38 -0400

    feat: Startup a KOOK Bot Client!

--- src/main/kotlin/top/cubik65536/Application.kt
+++ src/main/kotlin/top/cubik65536/Application.kt
@@ -2,8 +2,13 @@ package top.cubik65536
 
 import de.comahe.i18n4k.config.I18n4kConfigDefault
 import de.comahe.i18n4k.i18n4k
+import io.github.kookybot.client.Client
+import io.github.kookybot.events.channel.ChannelMessageEvent
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import top.cubik65536.entity.ConfigEntity
 import top.cubik65536.plugins.*
 import top.cubik65536.util.VersionUtil
@@ -27,6 +32,14 @@ fun main() {
         println("${I18N.releaseCandidate}")
     }
     println()
+
+    val kookToken = config.kook.token
+    val kookClient = Client(kookToken) {
+        withDefaultCommands()
+    }
+    CoroutineScope(Dispatchers.Default).launch {
+        val self = kookClient.start()
+    }
 
     embeddedServer(Netty, port = config.ktor.port, host = config.ktor.host) {
         configureRouting()
